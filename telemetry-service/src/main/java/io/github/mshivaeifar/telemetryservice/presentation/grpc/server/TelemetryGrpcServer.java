f7120a40f37cd3b9b9f6522c576ebf4dbde9dedc
Mahdi Shivaeifar <shivaeifar.m@gmail.com>
2026-06-17 12:28:42 +0330

    feat: implement telemetry gRPC service endpoint

---
+++ telemetry-service/src/main/java/io/github/mshivaeifar/telemetryservice/presentation/grpc/server/TelemetryGrpcServer.java
@@ -0,0 +1,38 @@
+package io.github.mshivaeifar.telemetryservice.presentation.grpc.server;
+
+import io.github.mshivaeifar.telemetryservice.application.service.TelemetryQueryService;
+import io.github.mshivaeifar.telemetryservice.domain.model.DriverLocation;
+import io.github.mshivaeifar.telemetryservice.grpc.GetDriverLocationRequest;
+import io.github.mshivaeifar.telemetryservice.grpc.GetDriverLocationResponse;
+import io.github.mshivaeifar.telemetryservice.grpc.TelemetryServiceGrpc;
+import io.github.mshivaeifar.telemetryservice.presentation.grpc.mapper.TelemetryGrpcMapper;
+import io.grpc.stub.StreamObserver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.grpc.server.service.GrpcService;
+
+import java.util.UUID;
+
+@GrpcService
+@RequiredArgsConstructor
+public class TelemetryGrpcServer extends TelemetryServiceGrpc.TelemetryServiceImplBase {
+    private final TelemetryQueryService service;
+    private final TelemetryGrpcMapper mapper;
+
+    @Override
+    public void getDriverLocation(
+            GetDriverLocationRequest request,
+            StreamObserver<GetDriverLocationResponse> responseObserver
+    ) {
+
+        UUID driverId = UUID.fromString(request.getDriverId());
+
+        DriverLocation location =
+                service.getCurrentLocation(driverId);
+
+        GetDriverLocationResponse response =
+                mapper.toResponse(location);
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
