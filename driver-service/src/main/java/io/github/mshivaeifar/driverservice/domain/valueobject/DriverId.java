29ebeea44b388258bc14ff7752680ac10655521b
Mahdi Shivaeifar <shivaeifar.m@gmail.com>
2026-06-17 19:37:02 +0330

    refactor: convert driver id to a rich value object

--- driver-service/src/main/java/io/github/mshivaeifar/driverservice/domain/valueobject/DriverId.java
+++ driver-service/src/main/java/io/github/mshivaeifar/driverservice/domain/valueobject/DriverId.java
@@ -4,8 +4,41 @@ import java.util.Objects;
 import java.util.UUID;
 
 public record DriverId(UUID value) {
-    public static DriverId of(UUID value) {
+
+    public DriverId {
         Objects.requireNonNull(value, "DriverId cannot be null");
+    }
+
+    public static DriverId of(UUID value) {
+
         return new DriverId(value);
+    }
+
+    public UUID value() {
+        return this.value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o) {
+            return true;
+        }
+
+        if (!(o instanceof DriverId other)) {
+            return false;
+        }
+
+        return value.equals(other.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public String toString() {
+        return value.toString();
     }
 }
