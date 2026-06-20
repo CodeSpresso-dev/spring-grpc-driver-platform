8a5d11b1f2128fb8ba77911313131dcae5ff9953
Mahdi Shivaeifar <shivaeifar.m@gmail.com>
2026-06-20 13:20:09 +0330

    refactor: add constants class to hold constants value

---
+++ driver-service/src/main/java/io/github/mshivaeifar/driverservice/infrastructure/observability/tracing/ObservabilityConstants.java
@@ -0,0 +1,10 @@
+package io.github.mshivaeifar.driverservice.infrastructure.observability.tracing;
+
+public final class ObservabilityConstants {
+
+    private ObservabilityConstants() {
+    }
+
+    public final static String CORRELATION_ID_HEADER_NAME = "X-Correlation-Id";
+
+}
