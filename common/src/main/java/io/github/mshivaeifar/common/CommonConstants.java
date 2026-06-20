62ad10bb3024874c792e20112c6d292fb309894d
Mahdi Shivaeifar <shivaeifar.m@gmail.com>
2026-06-20 13:15:20 +0330

    refactor: add common constants static fields to share into all other modules

---
+++ common/src/main/java/io/github/mshivaeifar/common/CommonConstants.java
@@ -0,0 +1,7 @@
+package io.github.mshivaeifar.common;
+
+public final class CommonConstants {
+    private CommonConstants(){}
+
+    public final static String CORRELATION_ID = "correlation_id";
+}
